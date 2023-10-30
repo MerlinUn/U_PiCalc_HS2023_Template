e0d8545b7a27424707f0280d1be32048261ccf55
MerlinUn <merlin.unt@gmail.com>
2023-10-30 18:48:02 +0100

    Finished
    Finished some details. Code is now good enough.

--- U_PiCalc_HS2023/main.c
+++ U_PiCalc_HS2023/main.c
@@ -2,7 +2,7 @@
  * U_PiCalc_HS2023.c
  *
  * Created: 3.10.2023:18:15:00
- * Author : -
+ * Author : Merlin Unternährer
  */ 
 
 #include <math.h>
@@ -28,50 +28,45 @@
 #include "utils.h"
 #include "errorHandler.h"
 #include "NHD0420Driver.h"
+#include "avr_f64.h"
 
 #include "ButtonHandler.h"
 
 
 // Task handles and states
-TaskHandle_t vleibniz_tsk;       // Handle for Leibniz calculation task
-TaskHandle_t vnil_som_tsk;       // Handle for Nilakantha calculation task
-eTaskState taskStateLeibniz;    // State of the Leibniz calculation task
-eTaskState taskStateNilSom;     // State of the Nilakantha calculation task
+TaskHandle_t vLeibniz_tsk;			// Handle for Leibniz calculation task
+TaskHandle_t vNil_tsk;				// Handle for Nilakantha calculation task
+eTaskState taskStateLeibniz;		// State of the Leibniz calculation task
+eTaskState taskStateNilakantha;     // State of the Nilakantha calculation task
 
 // Function declarations
 void vControllerTask(void* pvParameters);
 void vCalculationTaskLeibniz(void* pvParameters);
-void vCalculationTaskNilakanthaSomayaji(void* pvParameters);
+void vCalculationTaskNilakantha(void* pvParameters);
 void vUi_task(void* pvParameters);
 
 // Event flags for button and task events
-#define EVBUTTONS_S1            1<<0 // Event flag for switching between Pi calculation algorithms
-#define EVBUTTONS_S2            1<<1 // Event flag for starting Pi calculation
-#define EVBUTTONS_S3            1<<2 // Event flag for stopping Pi calculation
-#define EVBUTTONS_S4            1<<3 // Event flag for resetting the selected algorithm
-#define EVBUTTONS_CLEAR         0xFF  // Used to clear button-related event flags
-EventGroupHandle_t evButtonEvents;   // Event group for button events
+#define EVBUTTONS_S1            1<<0	// Event flag for switching to the right between Pi calculation algorithm
+#define EVBUTTONS_S2            1<<1	// Event flag for starting Pi calculation
+#define EVBUTTONS_S3            1<<2	// Event flag for resetting the selected algorithm
+#define EVBUTTONS_S4            1<<3	// Event flag for switching to the left Pi calculation algorithm
+#define EVBUTTONS_CLEAR         0xFF	// Used to clear button-related event flags
+EventGroupHandle_t evButtonEvents;		// Event group for button events
 
-#define EVCALC_WAIT              1<<0 // Event flag for task waiting
-#define EVCALC_WAITING           1<<1 // Event flag indicating that a task is waiting
-EventGroupHandle_t evCalcTaskEvents; // Event group for task events
-uint32_t calcStateBits;              // Bits to track task state
+#define EVCALC_WAIT              1<<0	// Event flag for task waiting
+#define EVCALC_WAITING           1<<1	// Event flag indicating that a task is waiting
+EventGroupHandle_t evCalcTaskEvents;	// Event group for task events
+uint32_t calcStateBits;					// Bits to track task state
 
 TickType_t startTime, endTime;
 
-int time_ms = 0;      // Time in milliseconds
-float pi_approx = 0.0; // Approximation of Pi
+int time_ms = 0;						// Time in milliseconds
+float32_t pi_approx = 0.0;				// Approximation of Pi as float
 
-// Leibniz variables
-int iterations = 0;
-float term = 0.0;
-
-// Sign variable, used in both algorithms
+// Leibniz+Nilakantha variables
+uint32_t iterations = 0;
+float32_t term = 0.0;
 int sign = 1;
-
-// Nilakantha variables
-int numerator = 2;
-
 
 
 // Main function
@@ -86,13 +81,13 @@ int main(void) {
     
     // Create tasks
     xTaskCreate(vControllerTask, (const char*) "control_tsk", configMINIMAL_STACK_SIZE + 150, NULL, 3, NULL);
-    xTaskCreate(vCalculationTaskLeibniz, (const char*) "leibniz_tsk", configMINIMAL_STACK_SIZE + 300, NULL, 1, &vleibniz_tsk);
-    xTaskCreate(vCalculationTaskNilakanthaSomayaji, (const char*) "nil_som_tsk", configMINIMAL_STACK_SIZE + 300, NULL, 1, &vnil_som_tsk);
-    xTaskCreate(vUi_task, (const char*) "ui_tsk", configMINIMAL_STACK_SIZE + 50, NULL, 2, NULL);
+    xTaskCreate(vCalculationTaskLeibniz, (const char*) "leibniz_tsk", configMINIMAL_STACK_SIZE + 300, NULL, 1, &vLeibniz_tsk);
+    xTaskCreate(vCalculationTaskNilakantha, (const char*) "nilakantha_tsk", configMINIMAL_STACK_SIZE + 300, NULL, 1, &vNil_tsk);
+    xTaskCreate(vUi_task, (const char*) "ui_tsk", configMINIMAL_STACK_SIZE + 150, NULL, 2, NULL);
     
     // Suspend calculation tasks initially
-    vTaskSuspend(vnil_som_tsk);
-    vTaskSuspend(vleibniz_tsk);
+    vTaskSuspend(vNil_tsk);
+    vTaskSuspend(vLeibniz_tsk);
     
     // Start FreeRTOS scheduler
     vTaskStartScheduler();
@@ -148,8 +143,9 @@ void vCalculationTaskLeibniz(void* pvParameters) {
     }
 }
 
-void vCalculationTaskNilakanthaSomayaji(void* pvParameters) {
+void vCalculationTaskNilakantha(void* pvParameters) {
     for (;;) {
+		
         // Get task state bits for this task
         calcStateBits = (xEventGroupGetBits(evCalcTaskEvents)) & 0x000000FF;
 
@@ -167,10 +163,9 @@ void vCalculationTaskNilakanthaSomayaji(void* pvParameters) {
                 }
                 
                 // Update the approximation using the Nilakantha series
-                pi_approx += sign * (4.0 / (numerator * (numerator + 1) * (numerator + 2)));
-                sign = -sign;
-                numerator += 2;
-
+				pi_approx += sign * (4.0 / (2.0*iterations * (2.0*iterations + 1) * (2.0*iterations + 2)));
+				sign *= (-1);
+				iterations++;
                 if ((pi_approx > 3.14159 && pi_approx < 3.1416) && time_ms == 0) {
                     // Calculate and store the time in milliseconds
                     endTime = xTaskGetTickCount();
@@ -210,8 +205,8 @@ void vControllerTask(void* pvParameters) {
 }
 
 // UI Modes for the finite state machine
-#define UIMODE_INIT             0
-#define UIMODE_NIL_SOM_CALC      1
+#define UIMODE_INIT				 0
+#define UIMODE_NILAKANTHA_CALC   1
 #define UIMODE_LEIBNIZ_CALC      2
 
 uint8_t uiMode = UIMODE_INIT;
@@ -219,32 +214,33 @@ uint8_t uiMode = UIMODE_INIT;
 //vUi_task -> to handle the UI
 
 void vUi_task(void* pvParameters) {
-	char pistring[20];      // Character array to store the formatted value of pi
-	char timeString[20];    // Character array to store the formatted time in milliseconds
+	char pistring[20];			// Character array to store the formatted value of pi
+	char timeString[20];		// Character array to store the formatted time in milliseconds
 	EventBits_t bitsCalcTskEv;  // Bitmask to store event flags related to calculation tasks
 
 	for (;;) {
-		// Get the state of the Leibniz and Nilakantha calculation tasks
-		eTaskState taskStateNilSom = eTaskGetState(vnil_som_tsk);
-		eTaskState taskStateLeibniz = eTaskGetState(vleibniz_tsk);
+		// Get the state of the Leibniz, Nilakantha calculation tasks
+		eTaskState taskStateNilakantha = eTaskGetState(vNil_tsk);
+		eTaskState taskStateLeibniz = eTaskGetState(vLeibniz_tsk);		
 
 		// Set the EVCALC_WAIT bit in the event group to signal calculation tasks to wait
 		xEventGroupSetBits(evCalcTaskEvents, EVCALC_WAIT);
 
 		// If both calculation tasks are not suspended, wait for the EVCALC_WAITING event flag
-		if (!((taskStateLeibniz == eSuspended) && (taskStateNilSom == eSuspended))) {
+		if (!((taskStateLeibniz == eSuspended) && (taskStateNilakantha == eSuspended))) {
 			bitsCalcTskEv = xEventGroupWaitBits(evCalcTaskEvents, EVCALC_WAITING, pdTRUE, pdTRUE, portMAX_DELAY);
 		}
 
 		// Check if the EVCALC_WAITING event flag is set or both tasks are suspended
-		if ((bitsCalcTskEv & EVCALC_WAITING) || ((taskStateLeibniz == eSuspended) && (taskStateNilSom == eSuspended))) {
+		if ((bitsCalcTskEv & EVCALC_WAITING) || ((taskStateLeibniz == eSuspended) && (taskStateNilakantha == eSuspended))) {
 			// Clear the display
 			vDisplayClear();
 			
 			// Format and store the value of pi and time in milliseconds
 			sprintf(&pistring[0], "PI: %.8f", pi_approx);
 			sprintf(&timeString[0], "Time: %.6d ms", time_ms);
-
+			
+			
 			// Get the state of the button events
 			uint32_t buttonState = (xEventGroupGetBits(evButtonEvents)) & 0x000000FF;
 			xEventGroupClearBits(evButtonEvents, EVBUTTONS_CLEAR);
@@ -259,7 +255,7 @@ void vUi_task(void* pvParameters) {
 				case UIMODE_LEIBNIZ_CALC:
 				// Clear the display and update it with Leibniz calculation information
 				vDisplayClear();
-				eTaskState taskStateLeibniz = eTaskGetState(vleibniz_tsk);
+				eTaskState taskStateLeibniz = eTaskGetState(vLeibniz_tsk);
 				vDisplayWriteStringAtPos(0, 0, "Leibniz-Reihe:");
 				vDisplayWriteStringAtPos(1, 0, "%s", pistring);
 				vDisplayWriteStringAtPos(2, 0, "%s", timeString);
@@ -269,59 +265,58 @@ void vUi_task(void* pvParameters) {
 					vDisplayWriteStringAtPos(3, 4, "Start");
 					vDisplayWriteStringAtPos(3, 0, "|<|");
 					vDisplayWriteStringAtPos(3, 17, "|>|");
-					vDisplayWriteStringAtPos(3, 11, "|Reset");
+					vDisplayWriteStringAtPos(3, 10, "|Reset");
 					} else {
-					vDisplayWriteStringAtPos(3, 4, "Stop |");
+					vDisplayWriteStringAtPos(3, 4, "Stop");
 					vDisplayWriteStringAtPos(3, 0, "| |");
 					vDisplayWriteStringAtPos(3, 17, "| |");
-					vDisplayWriteStringAtPos(3, 11, "|     ");
+					vDisplayWriteStringAtPos(3, 10, "|Reset");
 				}
 
 				// Handle button events
 				if (buttonState & EVBUTTONS_S1) {
 					if (taskStateLeibniz == eSuspended) {
 						// Reset Leibniz calculation variables and switch to Nilakantha calculation
-						pi_approx = 0.0;
+						pi_approx = 0.0;				
 						term = 0.0;
-						iterations = 0;
+						iterations = 1;
 						sign = 1;
 						time_ms = 0;
-						uiMode = UIMODE_NIL_SOM_CALC;
+						uiMode = UIMODE_NILAKANTHA_CALC;
 					}
 				}
 				if (buttonState & EVBUTTONS_S2) {
 					if (taskStateLeibniz == eSuspended) {
 						// Start or stop Leibniz calculation task
 						startTime = xTaskGetTickCount();
-						vTaskResume(vleibniz_tsk);
+						vTaskResume(vLeibniz_tsk);
 						} else {
-						vTaskSuspend(vleibniz_tsk);
+						vTaskSuspend(vLeibniz_tsk);
 					}
 				}
 				if (buttonState & EVBUTTONS_S3) {
-					if (taskStateLeibniz == eSuspended) {
 						// Reset Leibniz calculation variables
 						pi_approx = 0.0;
 						term = 0.0;
-						iterations = 0;
+						iterations = 1;
 						sign = 1;
 						time_ms = 0;
-					}
+						startTime = xTaskGetTickCount();						
 				}
 				if (buttonState & EVBUTTONS_S4) {
 					if (taskStateLeibniz == eSuspended) {
 						// Reset Leibniz calculation variables and switch to Nilakantha calculation
 						pi_approx = 0.0;
 						term = 0.0;
-						iterations = 0;
+						iterations = 1;
 						sign = 1;
 						time_ms = 0;
-						uiMode = UIMODE_NIL_SOM_CALC;
+						uiMode = UIMODE_NILAKANTHA_CALC;
 					}
 				}
 				break;
 
-				case UIMODE_NIL_SOM_CALC:
+				case UIMODE_NILAKANTHA_CALC:
 				// Clear the display and update it with Nilakantha calculation information
 				vDisplayClear();
 				vDisplayWriteStringAtPos(0, 0, "Nilakantha-Reihe:");
@@ -329,52 +324,51 @@ void vUi_task(void* pvParameters) {
 				vDisplayWriteStringAtPos(2, 0, "%s", timeString);
 
 				// Update UI elements based on the task state
-				if (taskStateNilSom == eSuspended) {
-					vDisplayWriteStringAtPos(3, 4, "Start|");
+				if (taskStateNilakantha == eSuspended) {
+					vDisplayWriteStringAtPos(3, 4, "Start");
 					vDisplayWriteStringAtPos(3, 0, "|<|");
 					vDisplayWriteStringAtPos(3, 17, "|>|");
-					vDisplayWriteStringAtPos(3, 11, "|Reset");
+					vDisplayWriteStringAtPos(3, 10, "|Reset");
 					} else {
-					vDisplayWriteStringAtPos(3, 4, "Stop |");
+					vDisplayWriteStringAtPos(3, 4, "Stop");
 					vDisplayWriteStringAtPos(3, 0, "| |");
 					vDisplayWriteStringAtPos(3, 17, "| |");
-					vDisplayWriteStringAtPos(3, 11, "|     ");
+					vDisplayWriteStringAtPos(3, 10, "|Reset");
 				}
 
 				// Handle button events
 				if (buttonState & EVBUTTONS_S1) {
-					if (taskStateNilSom == eSuspended) {
+					if (taskStateNilakantha == eSuspended) {
 						// Reset Nilakantha calculation variables and switch to Leibniz calculation
 						sign = 1;
-						numerator = 2;
+						iterations = 1;
 						pi_approx = 0.0;
 						time_ms = 0;
 						uiMode = UIMODE_LEIBNIZ_CALC;
 					}
 				}
 				if (buttonState & EVBUTTONS_S2) {
-					if (taskStateNilSom == eSuspended) {
+					if (taskStateNilakantha == eSuspended) {
 						// Start or stop Nilakantha calculation task
 						startTime = xTaskGetTickCount();
-						vTaskResume(vnil_som_tsk);
+						vTaskResume(vNil_tsk);
 						} else {
-						vTaskSuspend(vnil_som_tsk);
+						vTaskSuspend(vNil_tsk);
 					}
 				}
 				if (buttonState & EVBUTTONS_S3) {
-					if (taskStateNilSom == eSuspended) {
 						// Reset Nilakantha calculation variables
 						sign = 1;
-						numerator = 2;
+						iterations = 1;
 						pi_approx = 0.0;
 						time_ms = 0;
-					}
+						startTime = xTaskGetTickCount();						
 				}
 				if (buttonState & EVBUTTONS_S4) {
-					if (taskStateNilSom == eSuspended) {
-						// Reset Nilakantha calculation variables and switch to Leibniz calculation
+					if (taskStateNilakantha == eSuspended) {
+						// Reset Nilakantha calculation variables and switch to Wallis Product calculation
 						sign = 1;
-						numerator = 2;
+						iterations = 1;
 						pi_approx = 0.0;
 						time_ms = 0;
 						uiMode = UIMODE_LEIBNIZ_CALC;
